1a3bac1bfdc69a6fb0627efc3f6a5b2df006fcde
yy1a <63210693+yy1a@users.noreply.github.com>
2020-04-22 22:01:44 -0400

    Update receive.py

--- core/channels/wechat_utils/receive.py
+++ core/channels/wechat_utils/receive.py
@@ -1,40 +1,42 @@
 # -*- coding: utf-8 -*-#
 # filename: receive.py
 import xml.etree.ElementTree as ET
+from typing import Text
 
 
-def parse_xml(web_data):
-    '''
+def parse_xml(web_data: Text):
+    """
     input: web_data
     output: Msg_instance
-    '''
+    """
     if len(web_data) == 0:
         return None
     xmlData = ET.fromstring(web_data)
-    msg_type = xmlData.find('MsgType').text
-    if msg_type == 'text':
+    msg_type = xmlData.find("MsgType").text
+    if msg_type == "text":
         return TextMsg(xmlData)
-    elif msg_type == 'image':
+    elif msg_type == "image":
         return ImageMsg(xmlData)
 
 
 class Msg(object):
-    def __init__(self, xmlData):
-        self.ToUserName = xmlData.find('ToUserName').text
-        self.FromUserName = xmlData.find('FromUserName').text
-        self.CreateTime = xmlData.find('CreateTime').text
-        self.MsgType = xmlData.find('MsgType').text
-        self.MsgId = xmlData.find('MsgId').text
+    def __init__(self, xmlData: Text):
+        self.ToUserName = xmlData.find("ToUserName").text
+        self.FromUserName = xmlData.find("FromUserName").text
+        self.CreateTime = xmlData.find("CreateTime").text
+        self.MsgType = xmlData.find("MsgType").text
+        self.MsgId = xmlData.find("MsgId").text
 
 
 class TextMsg(Msg):
-    def __init__(self, xmlData):
+    def __init__(self, xmlData: Text):
         Msg.__init__(self, xmlData)
-        self.Content = xmlData.find('Content').text
+        self.Content = xmlData.find("Content").text
 
 
 class ImageMsg(Msg):
-    def __init__(self, xmlData):
+    def __init__(self, xmlData: Text):
         Msg.__init__(self, xmlData)
-        self.PicUrl = xmlData.find('PicUrl').text
-        self.Content = xmlData.find('MediaId').text
+        self.PicUrl = xmlData.find("PicUrl").text
+        self.Content = xmlData.find("MediaId").text
+
