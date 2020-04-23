aacadc7188ffa80ae5dfa63ac83b54c3f61a2686
yy1a <63210693+yy1a@users.noreply.github.com>
2020-04-22 22:01:08 -0400

    Update wechat.py

--- core/channels/wechat.py
+++ core/channels/wechat.py
@@ -22,54 +22,55 @@ class MessengerBot(OutputChannel):
     def name(cls) -> Text:
         return "wechat"
 
-    def __init__(self, 
-                wechat_secret: Optional[Text] = None, 
-                wechat_appid: Optional[Text] = None, 
-                automatic_mode: Optional[bool] = False
-                ) -> None:
-                
+    def __init__(
+        self,
+        wechat_secret: Optional[Text] = None,
+        wechat_appid: Optional[Text] = None,
+        automatic_mode: Optional[bool] = False,
+    ) -> None:
+
         self.wechat_secret = wechat_secret
         self.wechat_appid = wechat_appid
         self.exp_time = 0
-        self.access_token = ''
+        self.access_token = ""
         self.automatic_mode = automatic_mode
         self.automatic_response = []
         super().__init__()
 
     async def get_token(self):
         if time.time() > self.exp_time:
-            r = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}'
-                                                                              .format(self.wechat_appid, self.wechat_secret))
+            r = requests.get(
+                "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}".format(
+                    self.wechat_appid, self.wechat_secret
+                )
+            )
             d = json.loads(r.text)
-            self.access_token = d['access_token']
-            self.exp_time = time.time() + d['expires_in'] - 10  
-        return self.access_token    
+            self.access_token = d["access_token"]
+            self.exp_time = time.time() + d["expires_in"] - 10
+        return self.access_token
 
     def prepare_message(
-        self, recipient_id: Text, bot_reply: Text, msgtype: Text  
+        self, recipient_id: Text, bot_reply: Text, msgtype: Text
     ) -> Dict[Text, Any]:
         data = {
-                    "touser": recipient_id,
-                    "msgtype":msgtype,
-                    "text":
-                    {
-                        "content":bot_reply
-                    }
-                }
+            "touser": recipient_id,
+            "msgtype": msgtype,
+            "text": {"content": bot_reply},
+        }
         return data
 
     async def send(self, replyMsg: Dict[Text, Any]) -> None:
         """Sends a message to the recipient using the messenger client."""
         access_token = await self.get_token()
-        url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={}'.format(access_token)
-        send_response = requests.post(url,data=json.dumps(replyMsg))
+        url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={}".format(
+            access_token
+        )
+        send_response = requests.post(url, data=json.dumps(replyMsg))
 
         if send_response.status_code != requests.codes.ok:
             logger.error(
-                "Error trying to send wechat message. Response: %s",
-                send_response.text,
+                "Error trying to send wechat message. Response: %s", send_response.text,
             )
-
 
     async def send_text_message(
         self, recipient_id: Text, text: Text, **kwargs: Any
@@ -77,21 +78,20 @@ class MessengerBot(OutputChannel):
         """Send a message through this channel."""
         for message_part in text.strip().split("\n\n"):
             if not self.automatic_mode:
-                replyMsg = self.prepare_message(recipient_id, message_part, 'Text')
+                replyMsg = self.prepare_message(recipient_id, message_part, "Text")
                 await self.send(replyMsg)
             else:
-                self.automatic_response.append({'Text': text})
-    
+                self.automatic_response.append({"Text": text})
 
     async def send_image_url(
         self, recipient_id: Text, image: Text, **kwargs: Any
     ) -> None:
         """Sends an image. Default will just post the url as a string."""
         if not self.automatic_mode:
-            replyMsg = self.prepare_message(recipient_id, image, 'Image')
+            replyMsg = self.prepare_message(recipient_id, image, "Image")
             await self.send(replyMsg)
         else:
-            self.automatic_response.append({'Image': image})
+            self.automatic_response.append({"Image": image})
 
 
 class WechatInput(InputChannel):
@@ -111,11 +111,17 @@ class WechatInput(InputChannel):
             credentials.get("appid"),
             credentials.get("verify"),
             credentials.get("secret"),
-            credentials.get('customer_mode')
+            credentials.get("customer_mode"),
         )
         # pytype: enable=attribute-error
 
-    def __init__(self, wechat_appid: Text, wechat_verify: Text, wechat_secret: Text, customer_mode: bool) -> None:
+    def __init__(
+        self,
+        wechat_appid: Text,
+        wechat_verify: Text,
+        wechat_secret: Text,
+        customer_mode: bool,
+    ) -> None:
         """Create a wechat input channel.
 
         Args:
@@ -129,7 +135,6 @@ class WechatInput(InputChannel):
         self.wechat_verify = wechat_verify
         self.wechat_secret = wechat_secret
         self.customer_mode = customer_mode
-    
 
     def blueprint(
         self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
@@ -139,31 +144,29 @@ class WechatInput(InputChannel):
 
         @wechat_webhook.route("/webhook", methods=["GET"])
         async def token_verification(request: Request) -> HTTPResponse:
-            
+
             data = lambda x: request.args.get(x)
-            signature = data('signature')
-            timestamp = data('timestamp')
-            nonce = data('nonce')
-            echostr = data('echostr')
+            signature = data("signature")
+            timestamp = data("timestamp")
+            nonce = data("nonce")
+            echostr = data("echostr")
             token = self.wechat_verify
             list_ = [token, timestamp, nonce]
             list_.sort()
             sha1 = hashlib.sha1()
-            sha1.update(list_[0].encode('utf-8'))
-            sha1.update(list_[1].encode('utf-8'))
-            sha1.update(list_[2].encode('utf-8'))
+            sha1.update(list_[0].encode("utf-8"))
+            sha1.update(list_[1].encode("utf-8"))
+            sha1.update(list_[2].encode("utf-8"))
             hashcode = sha1.hexdigest()
-            print ("handle/GET func: hashcode, signature: ", hashcode, signature,token)
+            print("handle/GET func: hashcode, signature: ", hashcode, signature, token)
             if hashcode == signature:
                 return response.text(echostr)
             else:
                 logger.warning(
-                "Invalid wechat verify token! Make sure this matches "
-                "your webhook settings on the wechat app."
+                    "Invalid wechat verify token! Make sure this matches "
+                    "your webhook settings on the wechat app."
                 )
                 return response.text("failure, invalid token")
-        
-           
 
         @wechat_webhook.route("/webhook", methods=["POST"])
         async def webhook(request: Request) -> HTTPResponse:
@@ -171,12 +174,10 @@ class WechatInput(InputChannel):
             recMsg = parse_xml(xmldata)
 
             try:
-                automatic_mode = not self.customer_mode 
-            
+                automatic_mode = not self.customer_mode
+
                 out_channel = MessengerBot(
-                    self.wechat_secret,
-                    self.wechat_appid,
-                    automatic_mode
+                    self.wechat_secret, self.wechat_appid, automatic_mode
                 )
 
                 user_msg = UserMessage(
@@ -187,35 +188,34 @@ class WechatInput(InputChannel):
                 )
 
                 await on_new_message(user_msg)
-                
+
                 # there is no customer, so use automatic reply instead.
                 if not self.customer_mode:
-                    contents = out_channel.automatic_response #contents = ({'Text':'some text'},{'Image':'some image url'})
+                    contents = out_channel.automatic_response
                     if len(contents) > 1:
-                        get_val = lambda x:list(x.values())[0] #get value from {'Text': 'Some texe ot image url'}
-                        joint_msg = ' '.join([get_val(x) for x in contents]) # join all msgs together; 'Text1 Text2'
-                        content = {'Text': joint_msg}
+                        get_val = lambda x: list(x.values())[0]
+                        joint_msg = " ".join([get_val(x) for x in contents])
+                        content = {"Text": joint_msg}
                     else:
-                        content = contents[0] if contents else {'Text':'Hello!'}
-        
+                        content = contents[0] if contents else {"Text": "Hello!"}
+
                     toUser = recMsg.FromUserName
                     fromUser = recMsg.ToUserName
-                    if 'Text' in content:
-                        replyMsg = TextMsg(toUser, fromUser, content['Text'])
-                    elif 'Image' in content:
-                        replyMsg = ImageMsg(toUser, fromUser, content['Image'])
+                    if "Text" in content:
+                        replyMsg = TextMsg(toUser, fromUser, content["Text"])
+                    elif "Image" in content:
+                        replyMsg = ImageMsg(toUser, fromUser, content["Image"])
 
                     return response.text(replyMsg.send())
-
 
             except Exception as e:
                 logger.error(f"Exception when trying to handle message.{e}")
                 logger.debug(e, exc_info=True)
                 pass
-    
-            return response.text("success")
-        return wechat_webhook
 
+            return response.text("success")
+
+        return wechat_webhook
 
 
 
